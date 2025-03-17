52c44e3cc7d54fc65b4c94789aa1804d26fd677f
Reddyprasad <prasad20154@gmail.com>
2025-03-17 18:29:41 +0530

    This my git file

---
+++ git.java
@@ -0,0 +1,31 @@
+Jenkins: It is used to build and test, deploy the application. CI Tool, It is the father of the DevOps.
+
+CI/CD:Continuous Integration: 
+& Deployment 
+
+AWS: Amazon Web service--Provider-Cloud Computing 
+
+
+Git and Github:
+
+create a username and Email:
+
+$git config --global user.name "name"
+$git config --global user.email "email"
+
+Commands:
+
+git init-  when you run this command, it's create a sub directory under the main directory,
+
+.git- inside of this branches, configurations, hooks, objects, refs.
+
+git status- Display the state of the working directory.
+
+git add- to add all current changes (both modified and new files)to the staging area.
+
+git commit -m "message"- will commit the staging area to local repo
+
+git push- push the changes from local to remote repo
+
+git log- to check the history.
+_
