9aebf7dbbd1ec9244c8b4b6965794140953101e9
Malichetty Reddiprasad <100895948+prasaddevops2015@users.noreply.github.com>
2025-09-29 21:08:47 +0530

    Update git.java

--- git.java
+++ git.java
@@ -23,9 +23,20 @@ git status- Display the state of the working directory.
 
 git add- to add all current changes (both modified and new files)to the staging area.
 
-git commit -m "message"- will commit the staging area to local repo
+                                     git add <file name>
+
+git commit -m "message"- will commit the staging area to local repo--  It Is a Snapshot of our Project.
+
+                                     git commit -m "Message"
 
 git push- push the changes from local to remote repo
+                                     
+                                     git push -u origin >branch name>
 
-git log- to check the history.
+git log- to check the history
+                                    
+.
+                                     
+                                     
+                                          
 _
