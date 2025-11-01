9ba07de3951b5051b32f53a783283270f519228e
Malichetty Reddiprasad <100895948+prasaddevops2015@users.noreply.github.com>
2025-11-01 13:44:17 +0530

    Update git.java

--- git.java
+++ git.java
@@ -6,34 +6,7 @@ CI/CD:Continuous Integration:
 AWS: Amazon Web service--Provider-Cloud Computing 
 
 
-Git and Github:
 
-create a username and Email:
-
-$git config --global user.name "name"
-$git config --global user.email "email"
-
-Commands:
-
-git init-  when you run this command, it's create a sub directory under the main directory,
-
-.git- inside of this branches, configurations, hooks, objects, refs.
-
-git status- Display the state of the working directory.
-
-git add- to add all current changes (both modified and new files)to the staging area.
-
-                                     git add <file name>
-
-git commit -m "message"- will commit the staging area to local repo--  It Is a Snapshot of our Project.
-
-                                     git commit -m "Message"
-
-git push- push the changes from local to remote repo
-                                     
-                                     git push -u origin >branch name>
-
-git log- to check the history
 
 
                                      GitOps:
